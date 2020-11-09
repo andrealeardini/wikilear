b0afa64aa9b34dd0863054585292f3bcb21e27c2
Andrea Leardini <andrea@wikilear.it>
2020-11-09 10:44:32 +0100

    Customize eleventy configuration
    - Add passthrought for img and js
    - Add whatch for js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "dist",
-  },
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addWatchTarget("./src/js/");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
 };
