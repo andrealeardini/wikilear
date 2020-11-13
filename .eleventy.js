cef5fe0b1d3622ac6e8b2cfd5ed4678886931155
Andrea Leardini <andrea@wikilear.it>
2020-11-13 20:40:34 +0100

    Add filters and collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,47 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addWatchTarget("./src/js/");
+
+  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
+
+  let markdownIt = require("markdown-it");
+  let markdownItAttrs = require("markdown-it-attrs");
+  let options = {
+    html: true,
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
+
+  // Add filters
+  eleventyConfig.addFilter(
+    "dateFilter",
+    require("./src/_filters/date-filter.js")
+  );
+  eleventyConfig.addFilter(
+    "w3DateFilter",
+    require("./src/_filters/w3-date-filter.js")
+  );
+
+  // Returns a collection of blog posts in reverse date order
+  eleventyConfig.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/blog/**/*.md")].reverse();
+  });
+
+  // Returns a collection of articles in reverse date order filtered by feed
+  eleventyConfig.addCollection("feed", (collection) => {
+    return [...collection.getFilteredByGlob("./src/**/*.md")]
+      .filter((x) => x.data.feed)
+      .reverse();
+  });
+
   return {
+    markdownTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "dist",
+      data: "_data",
     },
   };
 };
