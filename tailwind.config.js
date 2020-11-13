039a5e92dc1af158753c4818f5e6212bbfe084c0
Andrea Leardini <andrea@wikilear.it>
2020-11-13 20:40:44 +0100

    Customize tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [
-    './src/**/*.md',
-    './src/**/*.njk',
-    './src/**/*.js',
-  ],
+  purge: ["./src/**/*.md", "./src/**/*.njk", "./src/**/*.js"],
   theme: {
     extend: {
       gridTemplateRows: {
-           // Site-specific row configuration
-         'layout': '60px 1fr 200px',
-        }
+        // Site-specific row configuration
+        layout: "60px 1fr 200px",
+      },
+      maxWidth: {
+        ch: "70ch",
+      },
     },
   },
-  variants: {},
+  variants: {
+    display: ({ after }) => after(["last"]),
+    margin: ({ after }) => after(["last"]),
+  },
   plugins: [],
-}
+};
