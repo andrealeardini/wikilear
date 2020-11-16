e293eb7a2888a7f6ec99800f28aae20acd293706
Andrea Leardini <andrea@wikilear.it>
2020-11-16 17:32:07 +0100

    Rename images folder

--- src/_data/cards.js
+++ src/_data/cards.js
@@ -1,14 +1,6 @@
 module.exports = [
   {
-    "title": "Blog",
-    "imageUrl": "/img/blog-post.svg",
-    "imageAlt": "",
-    "description": "Tutti gli articoli del blog"
+    imageUrl: "/images/blog-post.svg",
   },
   {
-    "title": "Tutorial",
-    "imageUrl": "/img/tutorial.svg",
-    "imageAlt": "",
-    "description": "Tutorial per imparare"
-  }
-]
+    imageUrl: "/images/tutorial.svg",
