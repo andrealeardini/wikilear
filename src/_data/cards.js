b850251b3997887df993dbd84e762b606292e8c0
Andrea Leardini <andrea@wikilear.it>
2020-11-16 17:32:38 +0100

    Add page url and other data

--- src/_data/cards.js
+++ src/_data/cards.js
@@ -1,6 +1,16 @@
 module.exports = [
   {
+    title: "Blog",
     imageUrl: "/images/blog-post.svg",
+    imageAlt: "",
+    more: "Leggi tutte le notizie...",
+    url: "/blog/",
   },
   {
+    title: "Tutorial",
     imageUrl: "/images/tutorial.svg",
+    imageAlt: "",
+    more: "Guarda tutti i tutorial...",
+    url: "/tutorial/",
+  },
+];
