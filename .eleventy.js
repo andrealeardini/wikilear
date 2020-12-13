ace08c6a56cf79d804811a6aca194cd4bebebf90
Andrea Leardini <andrea@wikilear.it>
2020-12-13 20:51:51 +0100

    Add eleventy-plugin-syntaxhighlight

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
 
   eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
+  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
   eleventyConfig.addPlugin(require("@11ty/eleventy-navigation"));
 
   let markdownIt = require("markdown-it");
