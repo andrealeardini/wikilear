c130b2ffabfcff8b7e9d038b585fde869fbf6b56
Andrea Leardini <andrea@wikilear.it>
2020-12-16 23:45:08 +0100

    Fix styles.css not found Styles.css is build by TailwindCSS, so it must run before eleventy. Use safelist to avoid purge remove classes added by eleventy plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   darkMode: "class",
   purge: {
     content: ["./src/**/*.md", "./src/**/*.njk", "./src/**/*.js"],
+    // direc-link class is added by markdown-anchor (eleventy) after purge process
+    options: {
+      safelist: ["direct-link"],
+    },
   },
   theme: {
     extend: {
