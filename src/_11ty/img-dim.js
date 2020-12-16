a01605dd2839a0e1703ad7cfb2878772c97c57e1
Andrea Leardini <andrea@wikilear.it>
2020-12-17 00:13:11 +0100

    Remove blurry-placeholder feature Don't work with transparent images

--- src/_11ty/img-dim.js
+++ src/_11ty/img-dim.js
@@ -1,4 +1,27 @@
 /**
+ * Copyright (c) 2020 Andrea Leardini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// customized version that remove blurry-placeholders.
+
+/**
  * Copyright (c) 2020 Google Inc
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -22,15 +45,13 @@
 const { JSDOM } = require("jsdom");
 const { promisify } = require("util");
 const sizeOf = promisify(require("image-size"));
-const blurryPlaceholder = require("./blurry-placeholder");
 const srcset = require("./srcset");
 const path = require("path");
 
 const ACTIVATE_AVIF = false;
 
 /**
- * Sets `width` and `height` on each image, adds blurry placeholder
- * and generates a srcset if none present.
+ * Sets `width` and `height` on each image and generates a srcset if none present.
  * Note, that the static `sizes` string would need to change for a different
  * blog layout.
  */
@@ -72,12 +93,6 @@ const processImage = async (img, outputPath) => {
     }px) min(calc(var(--main-width) * ${
       dimensions.height / dimensions.width
     }), ${dimensions.height}px)`;
-    img.setAttribute(
-      "style",
-      `background-size:cover;` +
-        `contain-intrinsic-size: ${containSize};` +
-        `background-image:url("${await blurryPlaceholder(src)}")`
-    );
     const doc = img.ownerDocument;
     const picture = doc.createElement("picture");
     const avif = doc.createElement("source");
