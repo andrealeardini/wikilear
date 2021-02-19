b7e3e846d7320331f6be554d60e5f9d78285df66
Andrea Leardini <andrea@wikilear.it>
2021-02-19 10:29:42 +0100

    Use a form for search component

--- src/_includes/components/search/search.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // gestisco il pulsante search
-  let searchBtn = document.getElementById("searchBtn");
-  let search = document.getElementById("search");
-  searchBtn.addEventListener("click", () => {
-    window.open("https://www.google.com/search?q=site:wikilear.it "+search.value);
-  });
-});
