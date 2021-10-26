4b6db9070a0d59a0f101bf51bc7e2d59b13f053d
Andrea Leardini <andrea@wikilear.it>
2021-10-26 10:46:44 +0200

    Fix formula buttons CSP

--- src/_includes/components/sheet/code.js
+++ src/_includes/components/sheet/code.js
@@ -1,24 +1,30 @@
-function showFormule(element) {
-  let cells = element.parentElement.getElementsByClassName("cells");
-  for (const cell of cells) {
-    if (cell.dataset.mode == "value") {
-      // set left alignement for formulas
-      if (cell.dataset.formula) {
-        cell.classList.add("text-left");
-        cell.innerHTML = cell.dataset.formula
+document.addEventListener("DOMContentLoaded", () => {
+  let buttons = document.getElementsByClassName('formulaButton');
+  for (const button of buttons) {
+    button.addEventListener("click", () => {
+      let cells = button.parentElement.getElementsByClassName("cells");
+      for (const cell of cells) {
+        if (cell.dataset.mode == "value") {
+          // set left alignement for formulas
+          if (cell.dataset.formula) {
+            cell.classList.add("text-left");
+            cell.innerHTML = cell.dataset.formula
 
-      } else {
-        cell.innerHTML = cell.dataset.value;
+          } else {
+            cell.innerHTML = cell.dataset.value;
+          }
+          cell.dataset.mode = "formula";
+
+        } else {
+          cell.innerHTML = cell.dataset.value;
+          // remove left alignement to div element
+          // make effective the cell alignement
+          cell.classList.remove("text-left");
+          cell.dataset.mode = "value"
+        }
       }
-      cell.dataset.mode = "formula";
-
-    } else {
-      cell.innerHTML = cell.dataset.value;
-      // remove left alignement to div element
-      // make effective the cell alignement
-      cell.classList.remove("text-left");
-      cell.dataset.mode = "value"
+      button.innerHTML == "Formule" ? button.innerHTML = "Valori" : button.innerHTML = "Formule"
     }
+    );
   }
-  element.innerHTML == "Formule" ? element.innerHTML = "Valori" : element.innerHTML = "Formule"
-}
+});
